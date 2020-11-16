df963d41826f2560179b46261de639f44bc44fba
nish.gowda6@gmail.com <60044986+nishgowda@users.noreply.github.com>
2020-11-16 10:59:13 -0500

    travis updates

---
+++ CMakeFiles/facemask.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facemask.dir/src/main.cpp.o"
+  "facemask"
+  "facemask.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facemask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/facemask_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facemask_test.dir/src/tests.cpp.o"
+  "facemask_test"
+  "facemask_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facemask_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

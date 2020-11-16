1db66b00664bcac6c88f5ba328c4d3dbcb09ab00
nish.gowda6@gmail.com <60044986+nishgowda@users.noreply.github.com>
2020-11-16 10:49:33 -0500

    travis updates

--- src/tests.cpp
+++ src/tests.cpp
@@ -10,6 +10,10 @@
 #include <assert.h>
 
 int main(int argc, char **argv) {
+    // tests for corerect command line args
     assert(executeCameraMask(argv, argc) == 1);
+
+    // tests for incorrect command line args
+    assert(executeCameraMask(argv, 1) == 0);
     return 0;
 }
